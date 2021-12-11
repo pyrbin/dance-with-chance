b0e28e141ccbf6a440c94732cd42f995e4cf85f1
smultronbusken <fredholmisak@outlook.com>
2021-12-11 22:32:08 +0100

    added drag control for dice + meta files

--- Assets/Runtime/Dice/DiceController.cs
+++ Assets/Runtime/Dice/DiceController.cs
@@ -9,20 +9,33 @@ using static Dice;
 public class DiceController : MonoBehaviour
 {
     public Dice dice;
+    public PlayerHoldDrag holdDrag;
 
     public float cooldown = 0.3f;
     private float currentCooldown = 0f;
     private bool locked = false;
 
+    void Awake() {
+        holdDrag.Released += drag => DragReleased(drag);
+    }
+
     void Update() {
         if (locked) {
             currentCooldown += Time.deltaTime;
-            Debug.Log(currentCooldown);
             if (currentCooldown > cooldown) {
                 locked = false;
                 currentCooldown = 0f;
             }
         }
+    }
+
+    public void DragReleased(float2 drag) {
+        if (locked) return;
+        float angle = math.atan2(drag.x, drag.y)*180/math.PI;
+        if (angle >= -45 && angle <= 45) Flip(FlipDirection.Down);
+        if (angle >= -135 && angle <= -45) Flip(FlipDirection.Right);
+        if (angle >= 45 && angle <= 135) Flip(FlipDirection.Left);
+        if (angle <= -135 || angle >= 135) Flip(FlipDirection.Up);
     }
 
     public void OnKeyPressed(InputAction.CallbackContext context) {
@@ -33,7 +46,6 @@ public class DiceController : MonoBehaviour
             if (direction == Vector2.up) Flip(FlipDirection.Up); 
             if (direction == Vector2.right) Flip(FlipDirection.Right); 
             if (direction == Vector2.left) Flip(FlipDirection.Left); 
-
         }
     }
 
