d5ec1baeb12b05982151c596d4a67299a592cb5d
Jamesstahl98 <Jamesstahl556@gmail.com>
2021-12-12 19:19:21 +0100

    Fixat knappar till meny

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Integration");
+    }
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+    public void GoToMenu ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
